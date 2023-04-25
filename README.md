5a2cfba8797b3dc61f050c8a3b90f781162f2fa3
Otavio Augusto Grandsire Baroni <114299049+otaviob12@users.noreply.github.com>
2023-04-25 14:33:02 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# projeto-avaliacao
+ projeto avaliação
---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+var stars = document.querySelectorAll('.star-icon');
+                  
+document.addEventListener('click', function(e){
+  var classStar = e.target.classList;
+  if(!classStar.contains('ativo')){
+    stars.forEach(function(star){
+      star.classList.remove('ativo');
+    });
+    classStar.add('ativo');
+    console.log(e.target.getAttribute('data-avaliacao'));
+    document.getElementById('texto').innerHTML = 'Obrigado pela sua avaliação!'
+  }
+});
